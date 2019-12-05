140403ecb29185025bed8609e318dff18aa555e6
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-05 10:13:25 +0100

    changed class to use it with reactive form

--- src/app/nasa-pic.ts
+++ src/app/nasa-pic.ts
@@ -1,9 +1,18 @@
 export class NasaPic {
-    roverName: string;
-    camera: string;
-    sol: number;
-    earthDate: string;
-    landingDate: string;
-    launchingDate: string;
-    status: string;
+    roverName: string = '';
+    // camera: string = '';
+    year: string = '';
+    month: string = '';
+    day: string = '';
+    // sol: number;
+    // earthDate: string;
+    // landingDate: string;
+    // launchingDate: string;
+    // status: string;
 }
+
+export class PicRequest {
+    nasaPic: NasaPic;
+    requestType: any = '';
+    text: string = '';
+  }
