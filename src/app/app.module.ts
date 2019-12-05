e0a4ef719bf1d2ad327c3f756115a776cf32b746
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-05 10:08:35 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AboutComponent } from './about/about.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,23 +7,27 @@ import { NasaService } from './nasa.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
    declarations: [
       AppComponent,
-      AboutComponent,
       HeaderComponent,
       FooterComponent,
-      HomeComponent
+      HomeComponent,
+      AboutComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      BrowserAnimationsModule,
+      ReactiveFormsModule
    ],
    providers: [
       NasaService
