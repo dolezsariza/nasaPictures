e31dbced33c41e8a60ec774bf9c8bfdb92406ae6
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-05 10:11:02 +0100

    finished api request handler form and moved it to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { NasaService } from './../nasa.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { NasaPic, PicRequest } from '../nasa-pic';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  picForm: FormGroup;
   photos;
-  constructor(private nasaService: NasaService) { }
 
-  ngOnInit() {
-    this.nasaService.getPhotos().subscribe((data) => {
-      this.photos = data['photos'];
-      console.log(this.photos);
+  roverName = ['curiosity', 'opportunity', 'spirit'];
 
+  constructor(private formBuilder: FormBuilder, private nasaService: NasaService) {
+    this.picForm = this.createFormGroup(formBuilder);
+   }
+
+  createFormGroup(formBuilder: FormBuilder) {
+    return formBuilder.group({
+      nasaPic: formBuilder.group(new NasaPic())
     });
   }
 
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    const result: PicRequest = Object.assign({}, this.picForm.value);
+    result.nasaPic = Object.assign({}, result.nasaPic);
+
+    let url = this.nasaService.getUrl(result);
+    this.nasaService.getPhotos(url).subscribe((data) => {
+      this.photos = data['photos'];
+    });
+  }
+
+  revert() {
+
+    this.picForm.reset({ nasaPic: new NasaPic() });
+  }
 }
