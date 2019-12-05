227d34b48f5417bd49731084be385c2cb9bf227a
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-05 11:47:18 +0100

    added input validation to form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,27 @@
-import { NasaService } from './../nasa.service';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { NasaPic, PicRequest } from '../nasa-pic';
+import { NasaService } from "./../nasa.service";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { NasaPic, PicRequest } from "../nasa-pic";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
   picForm: FormGroup;
   photos;
+  choosenRoverName;
+  earthDate;
 
-  roverName = ['curiosity', 'opportunity', 'spirit'];
+  roverName = ["Curiosity", "Opportunity", "Spirit"];
 
-  constructor(private formBuilder: FormBuilder, private nasaService: NasaService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private nasaService: NasaService
+  ) {
     this.picForm = this.createFormGroup(formBuilder);
-   }
+  }
 
   createFormGroup(formBuilder: FormBuilder) {
     return formBuilder.group({
@@ -25,22 +29,26 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   onSubmit() {
     const result: PicRequest = Object.assign({}, this.picForm.value);
     result.nasaPic = Object.assign({}, result.nasaPic);
 
     let url = this.nasaService.getUrl(result);
-    this.nasaService.getPhotos(url).subscribe((data) => {
-      this.photos = data['photos'];
+    this.nasaService.getPhotos(url).subscribe(data => {
+      this.photos = data["photos"];
     });
+    this.choosenRoverName = result.nasaPic.roverName;
+    this.earthDate =
+      result.nasaPic.year +
+      "-" +
+      result.nasaPic.month +
+      "-" +
+      result.nasaPic.day;
   }
 
   revert() {
-
     this.picForm.reset({ nasaPic: new NasaPic() });
   }
 }
