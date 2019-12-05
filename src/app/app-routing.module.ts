4494a0997ece74c271fc04420ffd8c6aaedfe6b3
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-05 10:08:35 +0100

    Add components to json files, app-module and app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+
 
 
 const routes: Routes = [
