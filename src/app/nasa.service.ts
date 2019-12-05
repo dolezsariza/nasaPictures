a18c6a0810ab05e0fc2dfd4873af1f70d39869c0
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-05 11:49:18 +0100

    Use prettier to format Nasa.Service.ts

--- src/app/nasa.service.ts
+++ src/app/nasa.service.ts
@@ -1,22 +1,22 @@
-import { NasaPic } from './nasa-pic';
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { NasaPic } from "./nasa-pic";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class NasaService {
-  constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) {}
 
-  public firstPage = '';
-  public prevPage = '';
-  public nextPage = '';
-  public lastPage = '';
+  public firstPage = "";
+  public prevPage = "";
+  public nextPage = "";
+  public lastPage = "";
 
-  apiKey = 'gECkrxXkxPgh7m8P089iohsascxtdXzpExd9nBVK';
+  apiKey = "gECkrxXkxPgh7m8P089iohsascxtdXzpExd9nBVK";
   apiURL: string;
   apiURL2 = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=800&api_key=${this.apiKey}`;
 
@@ -32,10 +32,10 @@ export class NasaService {
     const month = result.nasaPic.month;
     const day = result.nasaPic.day;
 
+    // tslint:disable-next-line: max-line-length
     this.apiURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${year}-${month}-${day}&api_key=${this.apiKey}`;
-    return (this.apiURL);
-
-    }
+    return this.apiURL;
+  }
 
   /*public writeMessage(event: any) {
     let id = event.target.id;
@@ -54,5 +54,4 @@ export class NasaService {
     console.log(document.getElementById('photoModal').innerHTML);
 
   }*/
-
 }
